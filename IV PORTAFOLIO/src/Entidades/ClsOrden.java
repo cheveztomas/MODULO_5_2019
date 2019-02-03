2deb1bc041b616783e33167515ea49bbdc4d25ea
Tomás Andrés Chévez Elizondo <42824002+cheveztomas@users.noreply.github.com>
2019-02-02 22:36:11 -0600

    Update ClsOrden.java

--- IV PORTAFOLIO/src/Entidades/ClsOrden.java
+++ IV PORTAFOLIO/src/Entidades/ClsOrden.java
@@ -31,7 +31,75 @@ public class ClsOrden {
         vgn_maxComputadoras = pvn_maxComputadoras;
         vgo_Computadora = pvo_Computadora;
     }
-    
-    //Metodos
+
+    /**
+     * @return the vgn_idOrden
+     */
+    public int getVgn_idOrden() {
+        return vgn_idOrden;
+    }
+
+    /**
+     * @param vgn_idOrden the vgn_idOrden to set
+     */
+    public void setVgn_idOrden(int vgn_idOrden) {
+        this.vgn_idOrden = vgn_idOrden;
+    }
+
+    /**
+     * @return the vgn_contadorOrdenes
+     */
+    public int getVgn_contadorOrdenes() {
+        return vgn_contadorOrdenes;
+    }
+
+    /**
+     * @param vgn_contadorOrdenes the vgn_contadorOrdenes to set
+     */
+    public void setVgn_contadorOrdenes(int vgn_contadorOrdenes) {
+        this.vgn_contadorOrdenes = vgn_contadorOrdenes;
+    }
+
+    /**
+     * @return the vgn_contadorComputadoras
+     */
+    public int getVgn_contadorComputadoras() {
+        return vgn_contadorComputadoras;
+    }
+
+    /**
+     * @param vgn_contadorComputadoras the vgn_contadorComputadoras to set
+     */
+    public void setVgn_contadorComputadoras(int vgn_contadorComputadoras) {
+        this.vgn_contadorComputadoras = vgn_contadorComputadoras;
+    }
+
+    /**
+     * @return the vgn_maxComputadoras
+     */
+    public int getVgn_maxComputadoras() {
+        return vgn_maxComputadoras;
+    }
+
+    /**
+     * @param vgn_maxComputadoras the vgn_maxComputadoras to set
+     */
+    public void setVgn_maxComputadoras(int vgn_maxComputadoras) {
+        this.vgn_maxComputadoras = vgn_maxComputadoras;
+    }
+
+    /**
+     * @return the vgo_Computadora
+     */
+    public ClsComputadora getVgo_Computadora() {
+        return vgo_Computadora;
+    }
+
+    /**
+     * @param vgo_Computadora the vgo_Computadora to set
+     */
+    public void setVgo_Computadora(ClsComputadora vgo_Computadora) {
+        this.vgo_Computadora = vgo_Computadora;
+    }
     
 }
