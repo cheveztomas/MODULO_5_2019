a4b392195c19802d80dd13129084438b4dc2b879
Tomás Andrés Chévez Elizondo <42824002+cheveztomas@users.noreply.github.com>
2019-02-02 22:10:23 -0600

    Create ClsOrden.java

---
+++ IV PORTAFOLIO/src/Entidades/ClsOrden.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author Thomas
+ */
+public class ClsOrden {
+
+    //Atributos
+    private int vgn_idOrden, vgn_contadorOrdenes, vgn_contadorComputadoras, vgn_maxComputadoras;
+    
+}
