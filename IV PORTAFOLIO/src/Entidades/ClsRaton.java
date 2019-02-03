ec92c5e61bf86622f7c40b158240832a3c6f3cbf
Tomás Andrés Chévez Elizondo <42824002+cheveztomas@users.noreply.github.com>
2019-02-02 20:08:22 -0600

    Create ClsRaton.java

---
+++ IV PORTAFOLIO/src/Entidades/ClsRaton.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author Thomas
+ */
+public class ClsRaton {
+
+    //Atributos
+    private int vgn_idRaton, vgn_contadorRatones;
+
+    //Constructores
+    public void ClsRatones() {
+        vgn_contadorRatones = 0;
+        vgn_idRaton = 0;
+    }
+    
+    
+}
