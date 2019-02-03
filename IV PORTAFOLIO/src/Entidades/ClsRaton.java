fd467429b92a62fda38270936f48fad80c743eea
Tomás Andrés Chévez Elizondo <42824002+cheveztomas@users.noreply.github.com>
2019-02-02 20:20:41 -0600

    Update ClsRaton.java

--- IV PORTAFOLIO/src/Entidades/ClsRaton.java
+++ IV PORTAFOLIO/src/Entidades/ClsRaton.java
@@ -16,12 +16,54 @@ public class ClsRaton {
 
     //Constructores
     public void ClsRatones() {
-        vgn_contadorRatones = 0;
-        vgn_idRaton = 0;
+        setVgn_contadorRatones(0);
+        setVgn_idRaton(0);
     }
 
     public void ClsRatones(int pvn_idRaton, int pvn_contadorRatones) {
-        vgn_contadorRatones = pvn_contadorRatones;
-        vgn_idRaton = pvn_idRaton;
+        setVgn_contadorRatones(pvn_contadorRatones);
+        setVgn_idRaton(pvn_idRaton);
     }
+
+    //Metodos
+    public String toString() {
+        //Variables
+        String vlc_Cadena = "";
+
+        //Inicio
+        vlc_Cadena = getVgn_contadorRatones() + ", " + getVgn_idRaton();
+
+        return vlc_Cadena;
+    }
+
+    //Propiedades
+
+    /**
+     * @return the vgn_idRaton
+     */
+    public int getVgn_idRaton() {
+        return vgn_idRaton;
+    }
+
+    /**
+     * @param vgn_idRaton the vgn_idRaton to set
+     */
+    public void setVgn_idRaton(int vgn_idRaton) {
+        this.vgn_idRaton = vgn_idRaton;
+    }
+
+    /**
+     * @return the vgn_contadorRatones
+     */
+    public int getVgn_contadorRatones() {
+        return vgn_contadorRatones;
+    }
+
+    /**
+     * @param vgn_contadorRatones the vgn_contadorRatones to set
+     */
+    public void setVgn_contadorRatones(int vgn_contadorRatones) {
+        this.vgn_contadorRatones = vgn_contadorRatones;
+    }
+    
 }
