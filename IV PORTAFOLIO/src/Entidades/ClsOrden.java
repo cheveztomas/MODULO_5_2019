30a102e7de91805799fdb3c882eeb9923e840c03
Tomás Andrés Chévez Elizondo <42824002+cheveztomas@users.noreply.github.com>
2019-02-02 22:16:27 -0600

    Update ClsOrden.java

--- IV PORTAFOLIO/src/Entidades/ClsOrden.java
+++ IV PORTAFOLIO/src/Entidades/ClsOrden.java
@@ -13,5 +13,25 @@ public class ClsOrden {
 
     //Atributos
     private int vgn_idOrden, vgn_contadorOrdenes, vgn_contadorComputadoras, vgn_maxComputadoras;
+    private ClsComputadora vgo_Computadora;
+
+    //Constructores
+    public ClsOrden() {
+        vgn_contadorComputadoras = 0;
+        vgn_contadorOrdenes = 0;
+        vgn_idOrden = 0;
+        vgn_maxComputadoras = 0;
+        vgo_Computadora = new ClsComputadora();
+    }
+
+    public ClsOrden(int pvn_idOrden, int pvn_contadorOrdenes, int pvn_contadorComputadoras, int pvn_maxComputadoras, ClsComputadora pvo_Computadora) {
+        vgn_contadorComputadoras = pvn_contadorComputadoras;
+        vgn_contadorOrdenes = pvn_contadorOrdenes;
+        vgn_idOrden = pvn_idOrden;
+        vgn_maxComputadoras = pvn_maxComputadoras;
+        vgo_Computadora = pvo_Computadora;
+    }
+    
+    //Metodos
     
 }
