cd923bc7ce5a804c14c752356dc6e83e92bc1787
Tomás Andrés Chévez Elizondo <42824002+cheveztomas@users.noreply.github.com>
2019-02-12 10:55:57 -0600

    Update ADClientes.java

--- Ejemplo/EjemploCapas/src/AccesoDatos/ADClientes.java
+++ Ejemplo/EjemploCapas/src/AccesoDatos/ADClientes.java
@@ -18,10 +18,19 @@ public class ADClientes {
     private Connection vgo_Conexion;
     private String vgc_Mensaje = "";
 
+    //Constructor
+    public ADClientes() throws Exception {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String url = "jdbc:sqlserver://localhost;databaseName=Ejemplo;user=sa;password=sa";
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
     //Propiedades
     public String getVgc_Mensaje() {
         return vgc_Mensaje;
     }
-    
-    
+
 }
