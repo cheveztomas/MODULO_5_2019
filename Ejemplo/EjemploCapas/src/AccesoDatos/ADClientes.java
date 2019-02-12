941faa6c8a4bd1b4cf4880060efbd496a3fc49b4
Tomás Andrés Chévez Elizondo <42824002+cheveztomas@users.noreply.github.com>
2019-02-12 10:31:10 -0600

    Create ADClientes.java

---
+++ Ejemplo/EjemploCapas/src/AccesoDatos/ADClientes.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AccesoDatos;
+
+import java.sql.*;
+import Entidades.*;
+
+/**
+ *
+ * @author tomas
+ */
+public class ADClientes {
+
+    //Atributos
+    private Connection vgo_Conexion;
+    private String vgc_Mensaje = "";
+
+    //Propiedades
+    public String getVgc_Mensaje() {
+        return vgc_Mensaje;
+    }
+    
+    
+}
