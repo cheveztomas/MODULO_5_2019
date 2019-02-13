86d849a99bc787fcd126a8f487d845293a2669fd
Tomás Andrés Chévez Elizondo <42824002+cheveztomas@users.noreply.github.com>
2019-02-12 20:03:40 -0600

    Update LogicaClientes.java

--- Ejemplo/EjemploCapas/src/Logica/LogicaClientes.java
+++ Ejemplo/EjemploCapas/src/Logica/LogicaClientes.java
@@ -5,6 +5,7 @@
  */
 package Logica;
 
+import AccesoDatos.ADClientes;
 import Entidades.ClsEntidadCliente;
 
 /**
@@ -15,7 +16,43 @@ public class LogicaClientes {
 
     public LogicaClientes() {
     }
-    public int InsertarCliente(ClsEntidadCliente pvo_EntidadCliente){
-        //Varaibles
+
+    public int InsertarCliente(ClsEntidadCliente pvo_EntidadCliente) {
+        //Variables
+        ADClientes vlo_AccesoDatos;
+        int vln_Resultado = 0;
+
+        //Inicio
+        try {
+            //Se instancia el acceso a datos.
+            vlo_AccesoDatos = new ADClientes();
+
+            //Se invoca el metodo.
+            vln_Resultado = vlo_AccesoDatos.Insertar(pvo_EntidadCliente);
+        } catch (Exception e) {
+            vln_Resultado = -1;
+            throw e;
+        } finally {
+            return vln_Resultado;
+        }
+    }
+
+    public int EliminarCliente(ClsEntidadCliente pvo_EntidadCliente) {
+        //Variables
+        ADClientes vlo_ADClientes;
+        int vln_Resultado = 0;
+
+        try {
+            //Se instancia el acceso a datos del cliente.
+            vlo_ADClientes = new ADClientes();
+
+            //Se invoca el metodo que elimina de la base de datos a un cliente.
+            vln_Resultado = vlo_ADClientes.eliminar(pvo_EntidadCliente);
+        } catch (Exception e) {
+            vln_Resultado = -1;
+            throw e;
+        } finally {
+            return vln_Resultado;
+        }
     }
 }
