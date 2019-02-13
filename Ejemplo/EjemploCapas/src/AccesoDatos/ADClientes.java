a2a16bfd7752316c7d288ce20207fe655b4b3947
Tomás Andrés Chévez Elizondo <42824002+cheveztomas@users.noreply.github.com>
2019-02-12 19:10:26 -0600

    v

--- Ejemplo/EjemploCapas/src/AccesoDatos/ADClientes.java
+++ Ejemplo/EjemploCapas/src/AccesoDatos/ADClientes.java
@@ -22,7 +22,7 @@ public class ADClientes {
     public ADClientes() throws Exception {
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            String url = "jdbc:sqlserver://localhost;databaseName=Ejemplo;user=sa;password=sa";
+            String url = "jdbc:sqlserver://tomaschevez.database.windows.net;databaseName=EJEMPLO;user=tomaschevez;password=sa";
             vgo_Conexion = DriverManager.getConnection(url);
         } catch (Exception e) {
             vgo_Conexion = null;
