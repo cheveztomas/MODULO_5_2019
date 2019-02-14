edb17e3be6d3b83ce98262b891b602b14aa6544b
Tomás Andrés Chévez Elizondo <42824002+cheveztomas@users.noreply.github.com>
2019-02-14 07:54:53 -0600

    v

--- Ejemplo/EjemploCapas/src/Entidades/ClsEntidaProducto.java
+++ Ejemplo/EjemploCapas/src/Entidades/ClsEntidaProducto.java
@@ -15,4 +15,44 @@ public class ClsEntidaProducto {
     private int vgn_idPorducto;
     private String vgc_Descripcion;
     private double vgn_Precio;
+
+    //Constructores
+    public ClsEntidaProducto(int vgn_idPorducto, String vgc_Descripcion, double vgn_Precio) {
+        this.vgn_idPorducto = vgn_idPorducto;
+        this.vgc_Descripcion = vgc_Descripcion;
+        this.vgn_Precio = vgn_Precio;
+    }
+
+    public ClsEntidaProducto() {
+        vgc_Descripcion = "";
+        vgn_Precio = 0;
+        vgn_idPorducto = 0;
+    }
+    
+    //Propiedades
+
+    public int getVgn_idPorducto() {
+        return vgn_idPorducto;
+    }
+
+    public void setVgn_idPorducto(int vgn_idPorducto) {
+        this.vgn_idPorducto = vgn_idPorducto;
+    }
+
+    public String getVgc_Descripcion() {
+        return vgc_Descripcion;
+    }
+
+    public void setVgc_Descripcion(String vgc_Descripcion) {
+        this.vgc_Descripcion = vgc_Descripcion;
+    }
+
+    public double getVgn_Precio() {
+        return vgn_Precio;
+    }
+
+    public void setVgn_Precio(double vgn_Precio) {
+        this.vgn_Precio = vgn_Precio;
+    }
+    
 }
