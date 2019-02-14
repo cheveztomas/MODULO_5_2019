b1b1fd77740b203321e0bfc2fb97ee99aaed905d
Tomás Andrés Chévez Elizondo <42824002+cheveztomas@users.noreply.github.com>
2019-02-13 21:56:26 -0600

    Create ClsEntidaProducto.java

---
+++ Ejemplo/EjemploCapas/src/Entidades/ClsEntidaProducto.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author Thomas
+ */
+public class ClsEntidaProducto {
+
+    //Atributos
+    private int vgn_idPorducto;
+    private String vgc_Descripcion;
+    private double vgn_Precio;
+}
