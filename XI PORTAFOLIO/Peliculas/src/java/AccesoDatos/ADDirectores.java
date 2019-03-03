2080be962adf3ecd81740d8d7bc115c27bd9fb8e
Tomás Andrés Chévez Elizondo <42824002+cheveztomas@users.noreply.github.com>
2019-03-03 14:43:14 -0600

    Update ADDirectores.java

--- XI PORTAFOLIO/Peliculas/src/java/AccesoDatos/ADDirectores.java
+++ XI PORTAFOLIO/Peliculas/src/java/AccesoDatos/ADDirectores.java
@@ -6,7 +6,11 @@
 package AccesoDatos;
 
 import Configuracion.Conexion;
+import Entidades.ClsDirectores;
+import Entidades.ClsRetorno;
+import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.Types;
 
 /**
  *
@@ -28,6 +32,49 @@ public class ADDirectores {
             throw e;
         }
     }
-    
-    
+
+    public ClsRetorno GuardarDirector(ClsDirectores pvo_Director) throws Exception {
+        //Variables
+        ClsRetorno vlo_Retorno = new ClsRetorno();
+        CallableStatement vlo_CS;
+
+        //Inicio
+        try {
+            vlo_CS = vgo_Connection.prepareCall("{call SP_GUARDAR_DIRECTORES(?,?,?)}");
+            vlo_CS.setInt(1, pvo_Director.getVgc_idDirector());
+            vlo_CS.setString(2, pvo_Director.getVgc_Nombre());
+            vlo_CS.setString(3, vlo_Retorno.getVgc_Mensaje());
+            vlo_CS.registerOutParameter(1, Types.INTEGER);
+            vlo_CS.registerOutParameter(3, Types.VARCHAR);
+            vlo_CS.executeUpdate();
+            vlo_Retorno.setVgc_Mensaje(vlo_CS.getString(3));
+            vlo_Retorno.setVgn_id(vlo_CS.getInt(1));
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            vgo_Connection = null;
+        }
+        return vlo_Retorno;
+    }
+
+    public ClsRetorno EliminarDirector(int pvn_idDirector) throws Exception {
+        //Varaibles
+        ClsRetorno vlo_Retorno = new ClsRetorno();
+        CallableStatement vlo_CS;
+
+        //Inicio
+        try {
+            vlo_CS = vgo_Connection.prepareCall("{call SP_ELIMINAR_DIRECTOR(?,?)}");
+            vlo_CS.setInt(1, pvn_idDirector);
+            vlo_CS.setString(2, vlo_Retorno.getVgc_Mensaje());
+            vlo_CS.registerOutParameter(3, Types.VARCHAR);
+            vlo_Retorno.setVgn_id(vlo_CS.executeUpdate());
+            vlo_Retorno.setVgc_Mensaje(vlo_CS.getString(2));
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            vgo_Connection = null;
+        }
+        return vlo_Retorno;
+    }
 }
